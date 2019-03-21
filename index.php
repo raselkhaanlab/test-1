ce23cf7ad54891d319ff25115994322124d77736
raselkhaanlab <raselkhaanlab@gmail.com>
2019-03-21 08:26:15 +0600

    bootstrap adding

--- index.php
+++ index.php
@@ -9,11 +9,18 @@
 <html>
 <head>
 	<title>Answer for question 1</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-	<table border="1px solid">
-		<thead>
-			<tr><th colspan="2"> Executing result form 1-100 </th></tr>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-2"></div>
+		<div class="col-md-8">
+			
+				<table class="table table-bordered table-hover table-striped table-responsive-md table-responsive-md">
+		<thead class="thead-dark">
+			<tr><th colspan="2" class="text-center"> Executing result form 1-100 </th></tr>
 			<tr>
 				<th>For key</th>
 				<th> executing value</th>
@@ -27,7 +34,18 @@
 
 				</tr>
 			<?php endforeach; ?>
+
+
 		</tbody>
 	</table>
+		</div>
+
+		<div class="col-md-2"></div>
+
+	</div>
+</div>
+
+
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
